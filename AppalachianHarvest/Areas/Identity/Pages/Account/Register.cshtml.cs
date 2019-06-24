feab7c75191037a54448435812e3c9d13ff3a4cf
sydneywait <sydneywait@gmail.com>
2019-06-24 12:22:36 -0400

    user can register and login, profile pic not working

--- AppalachianHarvest/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AppalachianHarvest/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -48,6 +48,11 @@ namespace AppalachianHarvest.Areas.Identity.Pages.Account
             [Required]
             [Display(Name = "Last Name")]
             public string LastName { get; set; }
+            public byte ProfilePicture { get; set; }
+            public bool IsCustomer { get; set; }
+            public bool IsEmployee { get; set; }
+            public bool IsSupervisor { get; set; }
+
 
 
 
@@ -78,7 +83,7 @@ namespace AppalachianHarvest.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { FirstName = Input.FirstName, LastName = Input.LastName, UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { FirstName = Input.FirstName, LastName = Input.LastName, UserName = Input.Email, Email = Input.Email, ProfilePicture=Input.ProfilePicture };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
