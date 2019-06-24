68267771ff3355a3eeb2186529353aa0fa509c16
sydneywait <sydneywait@gmail.com>
2019-06-24 11:23:47 -0400

    added application user:identity user and updated project to reference this class

--- AppalachianHarvest/Data/ApplicationDbContext.cs
+++ AppalachianHarvest/Data/ApplicationDbContext.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AppalachianHarvest.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppalachianHarvest.Data
 {
     public class ApplicationDbContext : IdentityDbContext
+
     {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
+
+
     }
+}
 }
---
+++ AppalachianHarvest/Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppalachianHarvest.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public byte ProfilePicture { get; set; }
+        public bool IsCustomer { get; set; }
+        public bool IsEmployee { get; set; }
+        public bool IsSupervisor { get; set; }
+
+        // Set up PK -> FK relationships to other objects
+    
+
+
+    }
+}
