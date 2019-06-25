d88515c18895982df52f64a67873306e1aa03184
sydneywait <sydneywait@gmail.com>
2019-06-25 09:48:47 -0400

    added product and shelf models

---
+++ AppalachianHarvest/Models/Shelf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppalachianHarvest.Models
+{
+    public class Shelf
+    {
+        public int ShelfId { get; set; }
+        public string Description { get; set} 
+    }
+}
